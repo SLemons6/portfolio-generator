a278053ea9f15b306b4c22fd4d8867449fab196c
Shayne Lemons <lemonsshayne6@gmail.com>
2021-09-28 09:59:08 -0700

    validate user input

--- app.js
+++ app.js
@@ -5,17 +5,46 @@ const promptUser = () => {
     return inquirer.prompt([{
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your github username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -34,12 +63,28 @@ Add a New Project
     return inquirer.prompt([{
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of your project?'
+                message: 'What is the name of your project? (Required)',
+                validate: projectNameInput => {
+                    if (projectNameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your project name!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'description',
-                message: 'Provide a description of the project (Required)'
+                message: 'Provide a description of the project (Required)',
+                validate: projectDescInput => {
+                    if (projectDescInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a description for your project!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'checkbox',
@@ -50,7 +95,15 @@ Add a New Project
             {
                 type: 'input',
                 name: 'link',
-                message: 'Enter the GitHub link to your project. (Required)'
+                message: 'Enter the GitHub link to your project. (Required)',
+                validate: githubLinkInput => {
+                    if (githubLinkInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the link for your github project!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'confirm',
